0c98cc35dd1e79ce61f3178e70d82bc65f2f083c
luyun <luyun@aidingmao.com>
2019-01-17 11:13:43 +0800

    加载更多/ 刷新追加数据

--- pages/home/note/index.js
+++ pages/home/note/index.js
@@ -11,14 +11,15 @@ Page({
     // 文章列表
     notes: [],
     emptyType: Empty.loading,
+    // 没有更多了
+    nomore: false
   },
 
   pageable: {
     // 发起分页请求
     loading: false,
     page: 1,
-    size: 10,
-    nomore: false
+    size: 10
   },
 
   setPageable: function(pageable){
@@ -30,9 +31,6 @@ Page({
     }
     if(pageable.size != null){
       this.pageable.size = pageable.size;
-    }
-    if(pageable.nomore != null){
-      this.pageable.nomore = pageable.nomore;
     }
   },
 
@@ -78,7 +76,7 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    this.fetchNoteItems(1);
+    this.fetchNoteItems(true);
   },
 
   /**
@@ -95,7 +93,12 @@ Page({
 
   },
 
-  fetchNoteItems: function (curPage=this.pageable.page) {
+  /**
+   * 
+   * @param {当前页} curPage 
+   * @param {是否追加数据，true会忽略curPage} append 
+   */
+  fetchNoteItems: function (curPage=this.pageable.page, append = false) {
     if(this.pageable.loading){
       return;
     }
@@ -104,32 +107,47 @@ Page({
     });
     let that = this;
     request.post("/inread-api/note/list"
-    , {page: curPage, size: this.pageable.size})
+    , { 
+      page: curPage,
+      size: this.pageable.size,
+      append: append,
+      timestamp: that.data.notes.length > 0 ? that.data.notes[0].timestamp : 0
+    })
     .then(res => {
-      if(curPage == 1){
-        that.setData({
-          emptyType: Empty.content
-        });
-        that.setData({
-          notes: []
-        });
-      }
+      // 有数据则追加
       if(res.data.data && res.data.data.records){
         that.setData({
           notes: that.data.notes.concat(res.data.data.records)
         });
       }
-      that.setPageable({
-        page: curPage + 1
-      });
+      // 初始化页面
+      if(curPage == 1){
+        if(that.data.notes.length == 0){
+          that.setData({
+            emptyType: Empty.empty
+          });
+        }else{
+          that.setData({
+            emptyType: Empty.content
+          });
+        }
+      }
+      if(res.data.data && res.data.data.pages == curPage){
+        that.setData({
+          nomore: true
+        });
+      }
+      if(!append){
+        that.setPageable({
+          page: curPage + 1
+        });
+      }
       console.log(res);
     }).catch(error => {
       if(curPage == 1){
         that.setData({
           emptyType: Empty.error
         });
-      }else{
-
       }
       console.error(error);
     }).finally(() => {
